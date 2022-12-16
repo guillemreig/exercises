7c954402d339ec2ae711c7883595b0ae1aea48eb
guillemreig <guillemreiggrau@gmail.com>
2022-12-16 12:34:31 +0100

    First commit

---
+++ JavaScript/Notes/Arrays/script.js
@@ -0,0 +1,45 @@
+// ARRAY METHODS
+
+// CREATES NEW ARRAY
+
+// .map()
+
+// .filter()
+
+// .join()
+
+// .concat()
+
+// .flat()
+
+// .slice()
+
+// EDITS ARRAY
+
+// .forEach
+
+// .pop()
+
+// .push()
+
+// .shift()
+
+// .unshift()
+
+// .sort()
+
+// .fill()
+
+// SEARCHES IN ARRAY
+
+// .find()
+
+// .findIndex()
+
+// .indexOf()
+
+// .some()
+
+// .every()
+
+// .includes()
