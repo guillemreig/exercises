d03812953a157f129091eceed3772e8b9b546f2f
guillemreig <guillemreiggrau@gmail.com>
2023-01-16 10:55:49 +0100

    Added array to ranges exercise

--- JavaScript/Exercises/script.js
+++ JavaScript/Exercises/script.js
@@ -270,3 +270,65 @@ function rgbPRO(r, g, b) {
 
     return toHex(r) + toHex(g) + toHex(b);
 } // Almost the same idea. I like the other way's return line more though
+
+// 4 kyu
+
+// Range extractor
+
+// Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+// Example: solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+// returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+// My attempt
+
+function solution(list) {
+    // 1. Creates an array of arrays made of ranges
+    let rangeArr = [];
+
+    let tempArr = [];
+    const listLength = list.length;
+
+    for (let i = 0; i < listLength; i++) {
+        tempArr.push(list[i]);
+        if (list[i + 1] - list[i] > 1 || list[i + 1] === undefined) {
+            rangeArr.push(tempArr);
+            tempArr = [];
+        }
+    }
+
+    console.log("checkpoint 1 :", rangeArr);
+
+    // TODO: complete solution
+
+    // 2. transforms individual arrays into ranges
+    function arrayToRange(arr) {
+        const arrLength = arr.length;
+        if (arrLength > 2) {
+            return arr[0] + "-" + arr[arrLength - 1];
+        } else {
+            return arr;
+        }
+    }
+
+    rangeArr = rangeArr.map((x) => arrayToRange(x));
+
+    console.log("checkpoint 2 :", rangeArr);
+
+    // 3. transforms rangeArray into string
+    rangeArr = rangeArr.flat().join(",");
+
+    console.log("checkpoint 3 :", rangeArr);
+
+    return rangeArr;
+}
+
+// Most voted solution
+function solutionPRO(list) {
+    for (var i = 0; i < list.length; i++) {
+        var j = i;
+        while (list[j] - list[j + 1] == -1) j++;
+        if (j != i && j - i > 1)
+            list.splice(i, j - i + 1, list[i] + "-" + list[j]);
+    }
+    return list.join();
+} // Turns out that the splice method can also replace what has been removed. This allows transforming the array on the go
