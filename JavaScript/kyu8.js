13257c385b17089f64c0937dae5d5d8a60a338d2
guillemreig <guillemreiggrau@gmail.com>
2023-03-23 10:35:06 +0100

    Added greeting exercise

--- JavaScript/kyu8.js
+++ JavaScript/kyu8.js
@@ -111,3 +111,17 @@ const joinMethod = (num) => [num].join("");
 const toFixedMethod = (num) => num.toFixed();
 
 const jsonStringify = (num) => JSON.stringify(num);
+
+// Returning Strings
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+const greet = (name) => `Hello, ${name} how are you doing today?`;
+
+// Interesting variations
+function greetReplace(name) {
+    return "Hello, <name> how are you doing today?".replace("<name>", name);
+}
+
+function greetASCII(name) {
+    return `\x48\x65\x6c\x6c\x6f\x2c\x20${name}\x20\x68\x6f\x77\x20\x61\x72\x65\x20\x79\x6f\x75\x20\x64\x6f\x69\x6e\x67\x20\x74\x6f\x64\x61\x79\x3f`;
+}
