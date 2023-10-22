79c8ac7790a1cace97badd43b57a5bf0e3a54875
guillemreig <guillemreiggrau@gmail.com>
2023-10-22 15:14:08 +0200

    Added kyu5 exercise

--- JavaScript/kyu5.js
+++ JavaScript/kyu5.js
@@ -179,3 +179,15 @@ function rgbPRO(r, g, b) {
 
     return toHex(r) + toHex(g) + toHex(b);
 } // Almost the same idea. I like the other way's return line more though
+
+// Moving Zeros To The End
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+// My first attempt
+const moveZeros = (arr) => [...arr.filter((x) => x !== 0), ...arr.filter((x) => x === 0)];
+
+// Another interesting solution
+function moveZeros(arr) {
+    return arr.sort((a, b) => (b === 0 ? -1 : 0));
+}
